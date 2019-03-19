1cdfd240f74994576ea7e3f2f9860f7fd9b94ecb
Hubert Wang <hubert.tatra@gmail.com>
2019-03-19 22:08:39 +0700

    Adjust post gameplay view layout

--- Sources/View/PostGameplayView.swift
+++ Sources/View/PostGameplayView.swift
@@ -10,6 +10,8 @@ public class PostGameplayView: UIView {
     // MARK: - Outlets
     var headerLabel: UILabel!
     var subheaderLabel: UILabel!
+    var thankYouIndonesianLabel: UILabel!
+    var thankYouEnglishLabel: UILabel!
     weak var delegate: PostGameplayViewDelegate?
     
     // MARK: - Lifecycle
@@ -19,6 +21,18 @@ public class PostGameplayView: UIView {
         self.backgroundColor = #colorLiteral(red: 0.1183903292, green: 0.6762167215, blue: 1, alpha: 0.2286494007)
         self.setupElements()
         self.delegate?.setupWinningAnimation()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+3) {
+            UIView.animate(withDuration: 1.5, animations: {
+                self.headerLabel.alpha = 1
+                self.subheaderLabel.alpha = 1
+            }, completion: { (_) in
+                UIView.animate(withDuration: 1.5, animations: {
+                    self.thankYouEnglishLabel.alpha = 1
+                    self.thankYouIndonesianLabel.alpha = 1
+                })
+            })
+        }
     }
     
     public required init?(coder aDecoder: NSCoder) {
@@ -34,7 +48,8 @@ public class PostGameplayView: UIView {
         self.headerLabel.textAlignment = .center
         self.headerLabel.text = "Bhinekka Tunggal Ika"
         self.headerLabel.sizeToFit()
-        self.headerLabel.center = self.center
+        self.headerLabel.frame = CGRect(x: self.center.x - self.headerLabel.frame.width/2, y: 130, width: self.headerLabel.frame.width, height: self.headerLabel.frame.height)
+        self.headerLabel.alpha = 0
         
         self.addSubview(self.headerLabel)
         
@@ -45,8 +60,36 @@ public class PostGameplayView: UIView {
         self.subheaderLabel.textAlignment = .center
         self.subheaderLabel.text = "Unite in diversity\nWe are different but we are one\nThis is the spirit of Indonesian"
         self.subheaderLabel.sizeToFit()
-        self.subheaderLabel.center = CGPoint(x: self.center.x, y: self.headerLabel.frame.maxY + self.subheaderLabel.frame.height/2 + 16)
+        self.subheaderLabel.frame = CGRect(x: self.center.x - self.subheaderLabel.frame.width/2, y: self.headerLabel.frame.maxY + 32, width: self.subheaderLabel.frame.width, height: self.subheaderLabel.frame.height)
+        self.subheaderLabel.alpha = 0
         
         self.addSubview(self.subheaderLabel)
+        
+        self.thankYouIndonesianLabel = UILabel()
+        self.thankYouIndonesianLabel.font = UIFont.systemFont(ofSize: 30, weight: .semibold)
+        self.thankYouIndonesianLabel.textColor = .black
+        self.thankYouIndonesianLabel.numberOfLines = 0
+        self.thankYouIndonesianLabel.textAlignment = .center
+        self.thankYouIndonesianLabel.text = "Terima kasih atas ulasan Anda!"
+        self.thankYouIndonesianLabel.sizeToFit()
+        print(self.thankYouIndonesianLabel.frame)
+        self.thankYouIndonesianLabel.frame = CGRect(x: self.center.x - self.thankYouIndonesianLabel.frame.width/2, y: self.subheaderLabel.frame.maxY + 64, width: self.thankYouIndonesianLabel.frame.width, height: self.thankYouIndonesianLabel.frame.height)
+        self.thankYouIndonesianLabel.alpha = 0
+        
+        self.addSubview(self.thankYouIndonesianLabel)
+        
+        self.thankYouEnglishLabel = UILabel()
+        self.thankYouEnglishLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        self.thankYouEnglishLabel.textColor = .gray
+        self.thankYouEnglishLabel.numberOfLines = 0
+        self.thankYouEnglishLabel.textAlignment = .center
+        self.thankYouEnglishLabel.text = "Thank you for reviewing my app!"
+        self.thankYouEnglishLabel.sizeToFit()
+        self.thankYouEnglishLabel.frame = CGRect(x: self.center.x - self.thankYouEnglishLabel.frame.width/2, y: self.thankYouIndonesianLabel.frame.maxY + 8, width: self.thankYouEnglishLabel.frame.width, height: self.thankYouEnglishLabel.frame.height)
+        self.thankYouEnglishLabel.alpha = 0
+        
+        self.addSubview(self.thankYouEnglishLabel)
+        
+        print(self.headerLabel.frame)
     }
 }
