06b80f7888ae00cdce86044b958b00fd1f4df3c5
Hubert Wang <hubert.tatra@gmail.com>
2019-03-22 22:23:33 +0700

    Add image source to model

--- Sources/Model/CardInformation.swift
+++ Sources/Model/CardInformation.swift
@@ -4,10 +4,12 @@ import UIKit
 public struct CardInformation {
     var title: String
     var description: String
+    var imageSource: String
     var isOpened = false
     
-    init(title: String, description: String) {
+    init(title: String, description: String, imageSource: String) {
         self.title = title
         self.description = description
+        self.imageSource = imageSource
     }
 }
